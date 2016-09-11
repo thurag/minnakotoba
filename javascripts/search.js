bf9bdc375c5f1d8acda903490605e6a64431c29d
ThuraAung <thura.ucsy@hotmail.com>
2016-09-11 23:51:05 +0900

    search draft design finished

--- javascripts/search.js
+++ javascripts/search.js
@@ -3,74 +3,25 @@ var notebooks = angular.module('notebooks', []);
 notebooks.controller('NotebookListCtrl', function($scope, $http) {
 
     $http.get('data/data.json')
-        .then(function(res){
-          console.log(res);
-      });
+    .then(function(res){
+          var vocaList = res.data.vocal,
+                vocals = [];
 
-    $scope.notebooks = [
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2011},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2010},
-    {"name": "Toshiba",
-    "procesor": "Intel core 2 duo",
-    "age": 2008},
-    {"name": "HP",
-    "procesor": "Intel core 2 duo",
-    "age": 2012},
-    {"name": "Acer",
-    "procesor": "AMD",
-    "age": 2006},
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2009},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2008},
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2011},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2010},
-    {"name": "Toshiba",
-    "procesor": "Intel core 2 duo",
-    "age": 2008},
-    {"name": "HP",
-    "procesor": "Intel core 2 duo",
-    "age": 2012},
-    {"name": "Acer",
-    "procesor": "AMD",
-    "age": 2006},
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2009},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2008},
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2011},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2010},
-    {"name": "Toshiba",
-    "procesor": "Intel core 2 duo",
-    "age": 2008},
-    {"name": "HP",
-    "procesor": "Intel core 2 duo",
-    "age": 2012},
-    {"name": "Acer",
-    "procesor": "AMD",
-    "age": 2006},
-    {"name": "Lenovo",
-    "procesor": "Intel i5",
-    "age": 2009},
-    {"name": "Toshiba",
-    "procesor": "Intel i7",
-    "age": 2008}
-    ];
-    $scope.orderList = "name";
+         for (var i = 0; i < vocaList.length; i++) {
+             chapterVocalList = vocaList[i].words;
+             for (var ii = 0; ii < chapterVocalList.length; ii++) {
+                var word = chapterVocalList[ii];
+                word.chapterNo = i + 1;
+                word.wordNo = ii + 1;
+                vocals.push(word);
+             }
+         }
+
+         $scope.vocals = vocals;
+
+         $scope.orderList = "chapterNo";
+
+// console.log(vocals);
+
+      });
 });
