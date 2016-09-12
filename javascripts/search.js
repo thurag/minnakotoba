05268f591e28ddbd7fcd9b22de4c47e6571a5828
ThuraAung <thura.ucsy@hotmail.com>
2016-09-12 22:40:23 +0900

    vocal id filter search performance added

--- javascripts/search.js
+++ javascripts/search.js
@@ -5,16 +5,17 @@ notebooks.controller('NotebookListCtrl', function($scope, $http) {
     $http.get('data/data.json')
     .then(function(res){
           var vocaList = res.data.vocal,
-                vocals = [];
+                vocals = [],
+                count = 0;
 
          for (var i = 0; i < vocaList.length; i++) {
              chapterVocalList = vocaList[i].words;
              for (var ii = 0; ii < chapterVocalList.length; ii++) {
                 var word = chapterVocalList[ii];
+                word.id = ++count;
                 word.chapterNo = i + 1;
                 word.wordNo = ii + 1;
                 vocals.push(word);
-console.log(word);
              }
          }
 
